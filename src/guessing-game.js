d24333cd8e74fc187af33ea7eb8490e547c15a93
Katiarynka <katia.tsygankova@gmail.com>
2021-09-04 18:43:12 +0300

    solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.begin = 0;
+        this.end = 0; 
+        this.result = 0; 
+    }
 
     setRange(min, max) {
-
+        this.begin = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.result = this.begin + Math.floor((this.end - this.begin + 1) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.end = this.result;
+        //console.log(`lower ${this.begin} ${this.end}`)
     }
 
     greater() {
-
+        this.begin = this.result;
+        //console.log(`greater ${this.begin} ${this.end}`)
     }
 }
 
